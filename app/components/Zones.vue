5a0f9ab36f0b2d80b225efb44f81367a2811fdf1
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-02-12 13:54:04 +0000

    map marker

--- app/components/Zones.vue
+++ app/components/Zones.vue
@@ -111,14 +111,7 @@ onMounted(async () => {
       // Nettoyer les anciens marqueurs si nécessaire (optionnel pour l'instant car l'initialisation suffit)
       citiesList.forEach(city => {
         var icon = leaflet.icon({
-            iconUrl: '/marker-icon-x2.png',
-            shadowUrl: '/marker-shadow.png',
-
-            iconSize:     [38, 95], // size of the icon
-            shadowSize:   [50, 64], // size of the shadow
-            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-            shadowAnchor: [4, 62],  // the same for the shadow
-            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+            iconUrl: '/marker-icon-x2.png'
         });
 
         const coords = city.lat && city.lng ? { lat: city.lat, lng: city.lng } : null
